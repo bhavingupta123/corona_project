161eb76a37510c6666bf5b8ec22eed07365fb073
bhavin <guptabhavin60@gmail.com>
2020-06-01 22:24:11 +0530

    first commit

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const newsleter = require('./models/newsletter')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 //require('dotenv').config();
-
+require('dotenv').config()
 var verify=0;
 var total,deaths,recovered;
 
 
-mongoose.connect("mongodb+srv://db:db123@cluster0-sgs6x.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 .then(() => {
@@ -31,7 +31,7 @@ mongoose.connect("mongodb+srv://db:db123@cluster0-sgs6x.mongodb.net/test?retryWr
 //artcle 1 end
 
 let xmlParser = require('xml2json');
-const newsapi = new NewsAPI('da5a2389155c443f8e00a9812ef591e7');
+const newsapi = new NewsAPI(process.env.API_KEY1);
 
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -48,12 +48,6 @@ app.get('/',(req,res)=>{
   axios({
     "method":"GET",
     "url":"https://api.rootnet.in/covid19-in/stats/latest",
-    /*"headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-    "useQueryString":true
-    }*/
     })
     .then((response)=>{
       console.log(response.data.data.summary.total)
@@ -229,147 +223,14 @@ app.post('/signupnews', (req, res) => {
       }
   })
 
-  // Construct req data
-  /*const data = {
-    members: [
-      {
-        email_address: email,
-        status: 'subscribed',
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-          ADDRESS:query1
-        }
-      }
-    ]
-  };
-
-  const postData = JSON.stringify(data);
-
-  fetch('https://us18.api.mailchimp.com/3.0/lists/3e474108b3', {
-    method: 'POST',
-    headers: {
-      Authorization: 'auth b609ff89d27cc1983ebfe71d42815353-us18'
-    },
-    body: postData
-  })
-    .then(res.statusCode === 200 ?
-      res.redirect('/newsupdates/success.html') :
-      res.redirect('/newsupdates/fail.html'))
-    .catch(err => console.log(err))*/
 })
 
-/*var options = {
-"method": "GET",
-"hostname": "corona-virus-world-and-india-data.p.rapidapi.com",
-"port": null,
-"path": "/api",
-"headers": {
-    "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
-    "x-rapidapi-key": "9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-    "useQueryString": true
-}
-};
-
-app.get('/', (req, res) => {
- console.log("hello")
-    var req = http.request(options, function (res) {
-        var chunks = [];
-        console.log("data")
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-        });
-    });
-
-    req.end();
-    console.log("end")
-});
-*/
-
 app.get('/statewise', (req, res) => {
-    
-    /*var respose= ' '
-    var body
-    var options={
-        host:'api.covid19api.com',
-        port : 80,
-        path:'/all',
-        method:'GET'
-    }
-
-    var req = http.request(options, function (res) {
-        var chunks = [];
-        console.log("data")
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-             body = Buffer.concat(chunks);
-            console.log(body.toString());
-        });
-    });
-
-    req.end();
-    console.log("end")
-    res.send(body)*/
-
-    /*WORKING HOSPITAL DATA
-    axios({
-        "method":"GET",
-        "url":"https://api.rootnet.in/covid19-in/hospitals/medical-colleges",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-        "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-        "useQueryString":true
-        }
-        })
-        .then((response)=>{
-          console.log(response.data.data.medicalColleges[0])
-          //res.render("results",{candidateList: response.data});
-        })
-        .catch((error)=>{
-          console.log(error)
-        }) 
-     WORKING HOSPITAL DATA   */
-
-
-    /*WORKING EMERGENCY NUMBER
-    axios({
-        "method":"GET",
-        "url":"https://api.rootnet.in/covid19-in/contacts",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-        "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-        "useQueryString":true
-        }
-        })
-        .then((response)=>{
-          console.log(response.data.data.contacts.regional[0])
-          //res.render("results",{candidateList: response.data});
-        })
-        .catch((error)=>{
-          console.log(error)
-        }) 
-    WORKING EMERGENCY NUMBER*/
     
      //WORKING CASES COUNT
     axios({
         "method":"GET",
         "url":"https://api.rootnet.in/covid19-in/stats/latest",
-        /*"headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-        "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-        "useQueryString":true
-        }*/
         })
         .then((response)=>{
           console.log(response.data.data.summary.total)
@@ -390,7 +251,7 @@ app.get('/emergency',(req, res)=>{
     "headers":{
     "content-type":"application/octet-stream",
     "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
+    "x-rapidapi-key":process.env.API_KEY2,
     "useQueryString":true
     }
     })
@@ -410,7 +271,7 @@ app.get('/hospital',(req, res)=>{
     "headers":{
     "content-type":"application/octet-stream",
     "x-rapidapi-host":"coronavirus-tracker-india-covid-19.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
+    "x-rapidapi-key":process.env.API_KEY3,
     "useQueryString":true
     }
     })
@@ -451,7 +312,7 @@ app.get('/news',(req,res)=>{
     "headers":{
     "content-type":"application/octet-stream",
     "x-rapidapi-host":"devru-times-of-india.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
+    "x-rapidapi-key":process.env.API_KEY4,
     "useQueryString":true
     },"params":{
     "feedtype":"sjson"
@@ -464,23 +325,6 @@ app.get('/news',(req,res)=>{
     .catch((error)=>{
       console.log(error)
     })
-
-/*axios({
-  "method":"GET",
-  //"url":"https://myallies-breaking-news-v1.p.rapidapi.com/GetTopNews",
-  "url":"https://news.google.com"
-  /*"headers":{
-  "content-type":"application/octet-stream",
-  "x-rapidapi-host":"myallies-breaking-news-v1.p.rapidapi.com",
-  "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-  "useQueryString":true
-  }
-  }) .then((response)=>{
-    console.log(response)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })*/
  
 })
 
@@ -492,7 +336,7 @@ app.get('/country',(req,res)=>{
     "headers":{
     "content-type":"application/octet-stream",
     "x-rapidapi-host":"covid-19-coronavirus-statistics2.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
+    "x-rapidapi-key":process.env.API_KEY5,
     "useQueryString":true
     }
     })
@@ -503,27 +347,7 @@ app.get('/country',(req,res)=>{
     .catch((error)=>{
       console.log(error)
     })
-  /*axios({
-    "method":"GET",
-    //"url":"https://timesofindia.indiatimes.com/rssfeeds/913168846.cms",
-    "url":"https://corona-api.com/countries",
-    //"url":"https://thevirustracker.com/free-api?global=stats",
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"devru-times-of-india.p.rapidapi.com",
-    "x-rapidapi-key":"9c6e1ba148mshbb87469eef4a471p140f66jsn7e00727e6331",
-    "useQueryString":true
-    },"params":{
-    "feedtype":"sjson"
-    }
-    })
-    .then((response)=>{
-      console.log(response.data.data)
-      res.render("country",{candidateList: response.data.data});
-    })
-    .catch((error)=>{
-      console.log(error)
-    })*/
+ 
 })
 
 const PORT = process.env.PORT || 8000;
